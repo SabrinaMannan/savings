016fbd2cc03c3871044a253f3697c531b9196412
SabrinaMannan <sabrinameem21@gmail.com>
2022-02-19 22:39:39 +0600

    js file edited

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//handling income and expenses
+function getInputValue(cost) {
+    const costInput = document.getElementById(cost);
+    const costNumber = parseInt(costInput.value);
+    return costNumber;
+}
+
+//total calculation
+function sumTotal() {
+    const incomeTotal = getInputValue('income');
+    const foodTotal = getInputValue('food');
+    const clothesTotal = getInputValue('clothes');
+    const rentTotal = getInputValue('rent');
+
+    const expenseTotal = foodTotal + clothesTotal + rentTotal;
+
+    const balancetotal = incomeTotal - expenseTotal;
+
+    const savingsTotal = balancetotal / 10;
+
+    document.getElementById('total-expense').innerText = expenseTotal;
+    document.getElementById('balance').innerText = balancetotal;
+    document.getElementById('savings').innerText = savingsTotal;
+
+}
